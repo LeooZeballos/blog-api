6e77035e9e7433fc9081fb49a77fe516e4de8b82
LeooZeballos <zeballosleonel3@gmail.com>
2023-07-08 17:14:07 -0300

    updated security config

--- src/main/java/com/leozeballos/blog/configuration/SecurityConfig.java
+++ src/main/java/com/leozeballos/blog/configuration/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.leozeballos.blog.configuration;
 
-import com.leozeballos.blog.repository.UserRepository;
-import com.leozeballos.blog.security.CustomUserDetailsService;
 import com.leozeballos.blog.security.JwtAuthenticationEntryPoint;
 import com.leozeballos.blog.security.JwtAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -24,26 +22,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private UserDetailsService userDetailsService;
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter();
-    }
-
-    @Autowired
-    public void setUserDetailsService(UserRepository userRepository, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
-        this.userDetailsService = new CustomUserDetailsService(userRepository);
-        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -55,10 +38,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authorizeRequests()
+                // Allow access to the home page, authentication endpoints and documentation
                 .antMatchers(HttpMethod.GET, "/").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
                 .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                // Only ADMIN can create, update and delete entries
+                .antMatchers(HttpMethod.POST, "/api/v1/entries").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/v1/entries/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/v1/entries/**").hasAuthority("ADMIN")
+                // Only authenticated users can access the API
                 .antMatchers("/api/v1/**").authenticated()
+                // Any other request must be authenticated
                 .anyRequest().authenticated();
         
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
@@ -67,6 +57,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
