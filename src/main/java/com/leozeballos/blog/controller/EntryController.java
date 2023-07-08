6e93f5f7c0cdd6703b49d5b75671aa21ceec91ff
LeooZeballos <zeballosleonel3@gmail.com>
2023-07-08 17:32:57 -0300

    better entry controller

--- src/main/java/com/leozeballos/blog/controller/EntryController.java
+++ src/main/java/com/leozeballos/blog/controller/EntryController.java
@@ -3,23 +3,27 @@ package com.leozeballos.blog.controller;
 import com.leozeballos.blog.dto.EntryDTO;
 import com.leozeballos.blog.dto.EntryResponse;
 import com.leozeballos.blog.exception.DuplicatedEntryException;
+import com.leozeballos.blog.exception.ResourceNotFoundException;
 import com.leozeballos.blog.service.EntryService;
 import com.leozeballos.blog.utility.AppConstants;
 
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.ValidationException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/" + AppConstants.API_VERSION + "/entries")
+@Slf4j
 public class EntryController {
 
-    private final EntryService entryService;
+    private final EntryService service;
 
     @GetMapping
     public EntryResponse getAllEntries(
@@ -27,36 +31,59 @@ public class EntryController {
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIR, required = false) String sortDir) {
-        return entryService.getAllEntries(pageNumber, pageSize, sortBy, sortDir);
+        return service.getAllEntries(pageNumber, pageSize, sortBy, sortDir);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EntryDTO> getEntryById(@PathVariable(name = "id") Long id) {
-        return ResponseEntity.ok(entryService.getEntryById(id));
+    public ResponseEntity<EntryDTO> getEntryById(
+            @PathVariable(name = "id") Long id
+    ) {
+        return ResponseEntity.ok(service.getEntryById(id));
     }
 
     @PostMapping
-    public ResponseEntity<?> saveEntry(@Valid @RequestBody EntryDTO entryDTO) {
+    public ResponseEntity<?> saveEntry(
+            @Valid @RequestBody EntryDTO entryDTO
+    ) {
         try {
-            EntryDTO dto = entryService.newEntry(entryDTO);
+            EntryDTO dto = service.newEntry(entryDTO);
+            log.info("Entry with id " + dto.getId() + " created successfully");
             return ResponseEntity.created(new java.net.URI("/api/" + AppConstants.API_VERSION + "/entries/" + dto.getId())).body(dto);
         } catch (DuplicatedEntryException e) {
+            log.error(e.toString());
             return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         } catch (Exception e) {
+            log.error(e.toString());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<EntryDTO> updateEntry(@Valid @RequestBody EntryDTO entryDTO, @PathVariable(name = "id") Long id) {
-        EntryDTO responseEntry = entryService.updateEntry(entryDTO, id);
-        return new ResponseEntity<>(responseEntry, HttpStatus.OK);
+    public ResponseEntity<?> updateEntry(
+            @Valid @RequestBody EntryDTO entryDTO,
+            @PathVariable(name = "id") Long id
+    ) {
+        try {
+            EntryDTO updatedEntry = service.updateEntry(entryDTO, id);
+            return ResponseEntity.ok(updatedEntry);
+        } catch (ResourceNotFoundException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.notFound().build();
+        } catch (ValidationException e) {
+            log.error("Validation failed for entry with ID {}: {}", id, e.getMessage());
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            log.error("An error occurred while updating entry with ID {}: {}", id, e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred");
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteEntry(@PathVariable(name = "id") Long id) {
-        entryService.deleteEntry(id);
-        return new ResponseEntity<>("Entry deleted", HttpStatus.OK);
+    public ResponseEntity<String> deleteEntry(
+            @PathVariable(name = "id") Long id
+    ) {
+        service.deleteEntry(id);
+        return ResponseEntity.ok("Entry with id " + id + " deleted successfully");
     }
 
 }
--- src/main/java/com/leozeballos/blog/dto/EntryDTO.java
+++ src/main/java/com/leozeballos/blog/dto/EntryDTO.java
@@ -20,11 +20,11 @@ public class EntryDTO {
     @Size(min = 2, message = "The title must be at least 2 characters long")
     private String title;
 
-    @NotEmpty(message = "The content cannot be empty")
+    @NotEmpty(message = "The description cannot be empty")
     @Size(min = 10, message = "The description must be at least 10 characters long")
     private String description;
 
-    @NotEmpty(message = "The email cannot be empty")
+    @NotEmpty(message = "The content cannot be empty")
     private String content;
 
     private Set<Comment> comments;
